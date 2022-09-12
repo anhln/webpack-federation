4f39b303a27bcf763fbff331a6fed2d36e30bd31
NALsocotecvn <ngoc-anh.le@socotec.com>
2022-09-12 08:25:12 +0700

    fix state

--- Shops/src/bootloader.js
+++ Shops/src/bootloader.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
+import { createStore } from "vuex";
 
 import "./index.css";
 
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const store = createStore({
+  state() {
+    return {
+      cartItems: 10,
+    };
+  },
+});
+
+app.use(store);
+app.mount("#app");
